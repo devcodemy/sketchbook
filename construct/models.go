cbfcf487653944b34cf01bec06bab33ec9fe65f6
devcodemy <devcodemy@gmail.com>
2022-11-23 00:06:09 +0200

    ...

--- construct/models.go
+++ construct/models.go
@@ -2,20 +2,15 @@ package construct
 
 type Inventory struct {
 	Applications []Application `json:"applications"`
-	// Applications map[string]Application `json:"applications"`
 }
 
 type Application struct {
-	AppName string `json:"appName,omitempty"`
-	// AppName     string            `json:"-"`
+	AppName     string            `json:"appName"`
 	Description string            `json:"description"`
 	Records     map[string]Record `json:"records"`
-	// Fields      map[string]Record `json:"datas"`
 }
 
 type Record struct {
-	// Name string      `json:"name"`
 	Name string      `json:"-"`
 	Data interface{} `json:"data"`
-	// Name string `json:"name"`
 }
