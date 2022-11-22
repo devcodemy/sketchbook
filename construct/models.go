2fafb2193ce0ea5ba44ebb281f9b50d8323056ee
devcodemy <devcodemy@gmail.com>
2022-11-23 00:03:48 +0200

    improve it

--- construct/models.go
+++ construct/models.go
@@ -6,14 +6,16 @@ type Inventory struct {
 }
 
 type Application struct {
-	AppName     string   `json:"appName,omitempty"`
-	Description string   `json:"description"`
-	Records     []Record `json:"records"`
+	AppName string `json:"appName,omitempty"`
+	// AppName     string            `json:"-"`
+	Description string            `json:"description"`
+	Records     map[string]Record `json:"records"`
 	// Fields      map[string]Record `json:"datas"`
 }
 
 type Record struct {
-	Name string      `json:"name"`
+	// Name string      `json:"name"`
+	Name string      `json:"-"`
 	Data interface{} `json:"data"`
 	// Name string `json:"name"`
 }
--- construct/test_models.go
+++ construct/test_models.go
@@ -6,24 +6,25 @@ import (
 )
 
 func TestThemAll() {
-	dt1 := []map[string]string{
-		{"user1": "password1"},
-		{"user2": "password2"},
+	dt1 := map[string]string{
+		"user1": "password1",
+		"user2": "password2",
 	}
 
 	dt2 := map[string]int32{
 		"someEntry1": 42,
 	}
 
-	dt3 := []map[int]string{
-		{1: "some-host-1.env.dc"},
-		{2: "some-host-2.env.dc"},
-		{3: "some-host-3.env.dc"},
-		{4: "some-host-4.env.dc"},
+	dt3 := map[int]string{
+		1: "some-host-1.env.dc",
+		2: "some-host-2.env.dc",
+		3: "some-host-3.env.dc",
+		4: "some-host-4.env.dc",
 	}
 
-	dt4 := []map[string]Record{
-		{"some-host-1.env.dc": Record{
+	// dt4 := []map[string]Record{
+	dt4 := map[string]Record{
+		"some-host-1.env.dc": {
 			Name: "Some Specs",
 			Data: map[string]int{
 				"cpu":  8,
@@ -31,8 +32,8 @@ func TestThemAll() {
 				"sdb1": 20,
 				"sdb2": 100,
 			},
-		}},
-		{"some-host-2.env.dc": Record{
+		},
+		"some-host-2.env.dc": {
 			Name: "Some Specs",
 			Data: map[string]interface{}{
 				"cpu":  8,
@@ -45,33 +46,38 @@ func TestThemAll() {
 					"lts":     true,
 				},
 			},
-		}},
+		},
 	}
 
 	rc1 := Record{
-		Name: "Some Data",
+		Name: "Some Data - rc1",
 		Data: dt1,
 	}
 
 	rc2 := Record{
-		Name: "Some Number",
+		Name: "Some Number - rc2",
 		Data: dt2,
 	}
 
 	rc3 := Record{
-		Name: "List of hosts",
+		Name: "List of hosts - rc3",
 		Data: dt3,
 	}
 
 	rc4 := Record{
-		Name: "Map Approach",
+		Name: "Map Approach - rc4",
 		Data: dt4,
 	}
 
 	ap1 := Application{
 		AppName:     "Some Cool Application",
 		Description: "Some Long Long Description",
-		Records:     []Record{rc1, rc2, rc3, rc4},
+		Records: map[string]Record{
+			rc1.Name: rc1,
+			rc2.Name: rc2,
+			rc3.Name: rc3,
+			rc4.Name: rc4,
+		},
 	}
 
 	in1 := Inventory{
